8527a31cde331617a325ab31a8059b0ff9b1c73c
Tadashi Saito <tad.a.digger@gmail.com>
2014-08-01 15:55:14 +0900

    implement demo api

--- app.rb
+++ app.rb
@@ -7,3 +7,18 @@ get '/ping' do
   content_type :json
   'pong'.to_json
 end
+
+def part_of_speech(node)
+  node.feature.split(',').first
+end
+
+get '/parse/:input' do |input|
+  @@tagger = MeCab::Light::Tagger.new
+
+  content_type :json
+  values = @@tagger.parse(input).map do |node|
+    [node.surface, part_of_speech(node)]
+  end
+  {parsed: values}.to_json
+end
+
